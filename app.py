3f7e36e86d7b63ff97d25d327c40d885ca23307f
Rangot <arty-a.2017@mail.ru>
2019-02-17 22:32:55 +0300

    changed get_task

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, request, url_for
 
 app = Flask(__name__)
 
@@ -21,7 +21,39 @@ tasks = [
 
 @app.route('/todo/api/v.1.0/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({'tasks': map(make_public_task, tasks)})
+
+
+@app.route('/todo/api/v.1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'tasks': task[0]})
+
+
+@app.route('/todo/api/v.1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or 'title' not in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get['description', ""],
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'tasks': tasks}), 201
+
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
 
 
 if __name__ == '__main__':
